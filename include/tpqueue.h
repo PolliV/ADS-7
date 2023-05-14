d2eb864a3f16beca8b80602449a5d3d345f07a23
PolliV <77505747+PolliV@users.noreply.github.com>
2023-05-14 12:52:03 +0300

    b54

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,50 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  private:
+    struct Item {
+      T item;
+      Item* next;
+    };
+    Item* head;
+    Item* tail;
+
+  public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    ~TPQueue() {
+      while (head) {
+        pop();
+      }
+    }
+    void push(const T& value) {
+      Item* data = new Item;
+      data->item = value;
+      data->next = nullptr;
+      if (head == nullptr) {
+        head = data;
+        tail = data;
+        return;
+      }
+      if (head->data.prior < data.prior) {
+        data->next = head;
+        head = data;
+        return;
+      }
+      Item* dop = head;
+      while (dop->next != nullptr && dop->next->item.prior >= data.prior) {
+        dop = dop->next;
+        data->next = dop->next;
+        dop->next = data;
+        if (data->next == nullptr) {
+          tail = data;
+        }
+      }
+    void T pop() {
+      Item* item = head->next;
+      T data = head->data;
+      delete head;
+      return data;
+    }
 };
 
 struct SYM {
